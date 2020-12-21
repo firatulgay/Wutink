29f6bb209fe093eb2acc794c6bde3add9e2750cc
firatulgay <Frt_1234>
2020-12-21 23:40:49 +0300

    test

--- src/main/java/com/fulgay/bilenyum/controllers/CategoryController.java
+++ src/main/java/com/fulgay/bilenyum/controllers/CategoryController.java
@@ -31,6 +31,9 @@ public class CategoryController {
         return categoryFacade.findAllCategories();
 
         //test mesaj
+        //masterrTessst
+        //masterrTessst
+
         //test mesaj
         //test mesaj
         //test mesaj
