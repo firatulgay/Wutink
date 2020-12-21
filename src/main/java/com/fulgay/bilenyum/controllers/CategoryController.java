c96511121376cbdda3c43ab00a4f1f68e1703825
firatulgay <Frt_1234>
2020-12-21 23:39:31 +0300

    testDevü

--- src/main/java/com/fulgay/bilenyum/controllers/CategoryController.java
+++ src/main/java/com/fulgay/bilenyum/controllers/CategoryController.java
@@ -31,6 +31,10 @@ public class CategoryController {
         return categoryFacade.findAllCategories();
 
         //test mesaj
+
+        //asdfsdfgsd
+        //asdasdas
+
         //test mesaj
         //test mesaj
         //test mesaj
