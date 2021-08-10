d114542502bfde9e83ccf3d051a038080fc3efda
firatulgay <firat.ulg@gmail.com>
2021-08-11 00:15:37 +0300

    hibernate fix

--- src/main/java/com/fulgay/wutink/dao/impl/UserDaoImpl.java
+++ src/main/java/com/fulgay/wutink/dao/impl/UserDaoImpl.java
@@ -4,6 +4,7 @@ import com.fulgay.wutink.dao.UserDao;
 import com.fulgay.wutink.domain.User;
 import com.fulgay.wutink.enums.EnumUserType;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
@@ -31,16 +32,21 @@ public class UserDaoImpl extends UserDao {
     @Override
     public User findUserByUserName(String userName) {
         Session session = getSession();
+        Transaction transaction = session.beginTransaction();
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<User> query = criteriaBuilder.createQuery(User.class);
         Root<User> root = query.from(User.class);
         query.select(root).where(criteriaBuilder.equal(root.get("userName"), userName));
 
         try {
-            return session.createQuery(query).getSingleResult();
-        }catch (NoResultException ex){
+            User singleResult = session.createQuery(query).getSingleResult();
+            transaction.commit();
+            return singleResult;
+
+        } catch (NoResultException ex) {
             return null;
-        }    }
+        }
+    }
 
     @Override
     public List<User> findAllByUserType(EnumUserType userType) {
@@ -52,8 +58,8 @@ public class UserDaoImpl extends UserDao {
 
         Session session = getSession();
         Query query = session.createQuery("select user from User user where userName = :username and password = :password");
-        query.setParameter("username",userName);
-        query.setParameter("password",password);
+        query.setParameter("username", userName);
+        query.setParameter("password", password);
 
         return (User) query.uniqueResult();
     }
